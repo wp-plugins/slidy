d829124cb74242e9fa06db90462259fae442e539
gungorbudak <gungorbudak@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-28 21:06:29 +0000

    Update adding CSS3 animation easing option
    git-svn-id: https://plugins.svn.wordpress.org/slidy/trunk@1077796 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: jQuery, slider, slick, slide, responsive, image, image album, image slider
 Donate link: http://www.gungorbudak.com
 Requires at least: 3.3
 Tested up to: 4.0
-Stable tag: 0.0.1
+Stable tag: 0.0.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- slidy.php
+++ slidy.php
@@ -3,7 +3,7 @@
  * Plugin Name: Slidy
  * Plugin URI: http://www.gungorbudak.com/slidy
  * Description: Slidy is a responsive jQuery slider that uses slick carousel and that is fully integrated into Wordpress
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Gungor Budak
  * Author URI: http://www.gungorbudak.com
  * License: GPL2
